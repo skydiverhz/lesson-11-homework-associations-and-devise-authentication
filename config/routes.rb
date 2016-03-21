a6c182800d0c562e5d44ab5235b5989b6773f5c7
skydiverhz <skydiver@pipeline.com>
2016-03-20 21:02:15 -0400

    installed devise gem, and devise views for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,17 @@
 Rails.application.routes.draw do
-  get 'tags/index'
 
-  get 'tags/new'
-
-  get 'tags/show'
-
-  get 'tags/edit'
-
-  get 'tags/delete'
-
-  get 'author/index'
-
-  get 'author/new'
-
-  get 'author/show'
-
-  get 'author/edit'
-
-  get 'author/delete'
-
-  get 'articles/new'
-
-  get 'articles/edit'
-
-  get 'articles/index'
-
-  get 'articles/show'
-
-  get 'articles/delete'
-
+  devise_for :users
  resources :articles do
     get :delete, :on => :member
   end
+
+resources :authors do
+    get :delete, :on => :member
+  end
+
+  resources :tags do
+    get :delete, :on => :member
+  end
+
   root 'articles#index'
 end
